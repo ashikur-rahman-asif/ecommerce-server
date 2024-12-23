85d0fea2fe3aa90b34ede54884ad48364f6b51a0
asif247 <ashikurrahmanasif247@yahoo.com>
2024-12-23 23:50:26 +0600

    user model done

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  userName: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    role: "user",
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
