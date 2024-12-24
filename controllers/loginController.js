7bd2f4344cb1b5ded5366afb1ab804e5934bb935
asif247 <ashikurrahmanasif247@yahoo.com>
2024-12-24 23:51:41 +0600

    cors correction

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,7 +1,11 @@
 const bcryptjs = require("bcryptjs");
-const jwt = require('jsonwebtoken');
-const User=require("../models/User")
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const login = async (req, res, next) => {
+  res.send("Hello login page");
+};
 
+module.exports = {
+  login,
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { register } = require("../controllers/registerController"); 
+const { register } = require("../controllers/registerController");
+const { login } = require("../controllers/loginController");
 
 const router = express.Router();
 
-
 router.post("/register", register);
+router.get("/login", login);
 
 module.exports = router;
