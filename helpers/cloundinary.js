7440dd72717aa5efd59154d9798c80276b894954
asif247 <ashikurrahmanasif247@yahoo.com>
2025-01-01 20:09:35 +0600

    image file upload helper function done

---
+++ helpers/cloundinary.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.C_NAME,
+  api_key: process.env.C_API_KEY,
+  api_secret: process.env.C_API_SECRET,
+});
+
+// Multer configuration (memory storage for direct upload to Cloudinary)
+const storage = multer.memoryStorage();
+const upload = multer({ storage }); // Middleware for handling file uploads
+
+// Function to handle image upload to Cloudinary
+const handleImageUpload = async (fileBuffer) => {
+  try {
+    const result = await cloudinary.uploader
+      .upload_stream(
+        {
+          resource_type: "auto", // Automatically detects file type
+        },
+        (error, result) => {
+          if (error) throw new Error(error.message);
+          return result;
+        }
+      )
+      .end(fileBuffer);
+
+    return result;
+  } catch (error) {
+    console.error("Cloudinary Upload Error:", error);
+    throw error;
+  }
+};
+
+module.exports = { upload, handleImageUpload };
