962e3d95f55d8ae605e75751b23544630399c3b1
asif247 <ashikurrahmanasif247@yahoo.com>
2025-01-02 01:03:39 +0600

    image upload correction

--- controllers/admin/products-controller.js
+++ controllers/admin/products-controller.js
@@ -2,20 +2,26 @@ const { imageUploadUtil } = require("../../helpers/cloundinary");
 
 const handleImageUpload = async (req, res) => {
   try {
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        message: "No file uploaded",
+      });
+    }
+
     const b64 = Buffer.from(req.file.buffer).toString("base64");
     const url = "data:" + req.file.mimetype + ";base64," + b64;
     const result = await imageUploadUtil(url);
-    console.log(result);
 
-    res.json({
+    res.status(200).json({
       success: true,
       result,
     });
   } catch (error) {
-    console.log(error);
-    res.json({
+    console.error("Error uploading image:", error);
+    res.status(500).json({
       success: false,
-      message: "Error occured",
+      message: "Internal Server Error",
     });
   }
 };
--- routes/admin/products-routes.js
+++ routes/admin/products-routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
 const { upload } = require("../../helpers/cloundinary");
-const { handleImageUpload } = require("../../controllers/admin/products-controller");
+const {
+  handleImageUpload,
+} = require("../../controllers/admin/products-controller");
+
 const router = express.Router();
 
-router.post("/upload-image", upload.single("file"), handleImageUpload);
-
+router.post("/upload-image", upload.single("my_file"), handleImageUpload);
 
 module.exports = router;
