b4ac67af6d01260f795718562accb56ea0a3039a
asif247 <ashikurrahmanasif247@yahoo.com>
2025-04-22 03:33:50 +0600

    getProductDetails controller done

--- controllers/shop/products-controllers.js
+++ controllers/shop/products-controllers.js
@@ -53,4 +53,28 @@ const getFilterProducts = async (req, res) => {
   }
 };
 
-module.exports = { getFilterProducts };
+const getProductDetails = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    if (!product)
+      return res.status(404).json({
+        success: false,
+        message: "Product not found!",
+      });
+
+    res.status(200).json({
+      success: true,
+      data: product,
+    });
+  } catch (e) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+};
+
+module.exports = { getFilterProducts, getProductDetails };
