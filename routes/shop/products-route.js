814efae348dcdfca4043e880c96f8a224b2029ee
asif247 <ashikurrahmanasif247@yahoo.com>
2025-04-28 18:05:14 +0600

    single product get route done

--- routes/shop/products-route.js
+++ routes/shop/products-route.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const {
   getFilterProducts,
+  getProductDetails,
 } = require("../../controllers/shop/products-controllers");
 
 const router = express.Router();
 
 router.get("/get", getFilterProducts);
+router.get("/get/:id", getProductDetails);
 
 module.exports = router;
