78f372bd9f47fbb25944648644283dd400f9c085
asif247 <ashikurrahmanasif247@yahoo.com>
2025-04-30 19:59:38 +0600

    basic controller setup done

---
+++ controllers/shop/cart-controller.js
@@ -0,0 +1,47 @@
+const addToCart = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "error",
+    });
+  }
+};
+const fetchCartItems = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "error",
+    });
+  }
+};
+const updateCartItems = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "error",
+    });
+  }
+};
+const deleteCartItems = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "error",
+    });
+  }
+};
+
+module.exports = {
+  addToCart,
+  fetchCartItems,
+  updateCartItems,
+  deleteCartItems,
+};
