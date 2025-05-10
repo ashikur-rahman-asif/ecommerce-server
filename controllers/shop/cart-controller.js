0906893e52df50be16129e3452b80d8d1691e1ca
asif247 <ashikurrahmanasif247@yahoo.com>
2025-05-10 22:26:13 +0600

    delete controller done

--- controllers/shop/cart-controller.js
+++ controllers/shop/cart-controller.js
@@ -5,18 +5,19 @@ const addToCart = async (req, res) => {
   try {
     const { userId, productId, quantity } = req.body;
 
-    if (!userId || !productId || !quantity <= 0) {
+    if (!userId || !productId || quantity <= 0) {
       return res.status(400).json({
         success: false,
-        message: "Invalid data provided",
+        message: "Invalid data provided!",
       });
     }
 
     const product = await Product.findById(productId);
+
     if (!product) {
       return res.status(404).json({
         success: false,
-        message: "Not Found",
+        message: "Product not found",
       });
     }
 
@@ -45,10 +46,11 @@ const addToCart = async (req, res) => {
     console.log(error);
     res.status(500).json({
       success: false,
-      message: "error",
+      message: "Error",
     });
   }
 };
+
 const fetchCartItems = async (req, res) => {
   try {
     const { userId } = req.params;
@@ -56,7 +58,7 @@ const fetchCartItems = async (req, res) => {
     if (!userId) {
       return res.status(400).json({
         success: false,
-        message: "User id is required",
+        message: "User id is manadatory!",
       });
     }
 
@@ -101,11 +103,12 @@ const fetchCartItems = async (req, res) => {
     console.log(error);
     res.status(500).json({
       success: false,
-      message: "error",
+      message: "Error",
     });
   }
 };
-const updateCartItems = async (req, res) => {
+
+const updateCartItemQty = async (req, res) => {
   try {
     const { userId, productId, quantity } = req.body;
 
@@ -163,24 +166,72 @@ const updateCartItems = async (req, res) => {
     console.log(error);
     res.status(500).json({
       success: false,
-      message: "error",
+      message: "Error",
     });
   }
 };
-const deleteCartItems = async (req, res) => {
+
+const deleteCartItem = async (req, res) => {
   try {
+    const { userId, productId } = req.params;
+    if (!userId || !productId) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid data provided!",
+      });
+    }
+
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "items.productId",
+      select: "image title price salePrice",
+    });
+
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "Cart not found!",
+      });
+    }
+
+    cart.items = cart.items.filter(
+      (item) => item.productId._id.toString() !== productId
+    );
+
+    await cart.save();
+
+    await cart.populate({
+      path: "items.productId",
+      select: "image title price salePrice",
+    });
+
+    const populateCartItems = cart.items.map((item) => ({
+      productId: item.productId ? item.productId._id : null,
+      image: item.productId ? item.productId.image : null,
+      title: item.productId ? item.productId.title : "Product not found",
+      price: item.productId ? item.productId.price : null,
+      salePrice: item.productId ? item.productId.salePrice : null,
+      quantity: item.quantity,
+    }));
+
+    res.status(200).json({
+      success: true,
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
       success: false,
-      message: "error",
+      message: "Error",
     });
   }
 };
 
 module.exports = {
   addToCart,
+  updateCartItemQty,
+  deleteCartItem,
   fetchCartItems,
-  updateCartItems,
-  deleteCartItems,
 };
